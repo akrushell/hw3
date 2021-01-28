6ff033d38ac0472c9cb74abcca0ed3c9d37b38c6
akrushell <76886773+akrushell@users.noreply.github.com>
2021-01-27 23:56:21 -0500

    Update noober.js

--- noober.js
+++ noober.js
@@ -6,7 +6,60 @@ function pageLoad() {
   // is refreshed or reloaded, the 'ride' variable will contain something different
   // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
   // e.g. ask 'ride' for its length or only grab the first member of the Array
+
+  //I tried using different if functions for different array lengths because now 
+  //the information below only shows up when there are three passengers but I couldn't figure it out 
+  
   console.log(ride)
+
+  passenger1Name = ride[0].passengerDetails.first
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = ride[0].numberofPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city
+
+  passenger2Name = ride[1].passengerDetails.first
+  passenger2Phone = ride[1].passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = ride[1].numberofPassengers
+  passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+  passenger2PickupAddressLine2 = ride[1].pickupLocation.city
+  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+  passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city
+
+  passenger3Name = ride[2].passengerDetails.first
+  passenger3Phone = ride[2].passengerDetails.phoneNumber
+  passenger3NumberOfPassengers = ride[2].numberofPassengers
+  passenger3PickupAddressLine1 = ride[2].pickupLocation.address
+  passenger3PickupAddressLine2 = ride[2].pickupLocation.city
+  passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
+  passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city 
+
+
+ if (ride.length > 1) {
+   levelOfService = "Noober Pool"
+ }
+
+ else if (ride[0].purpleRequested == true) {
+   levelOfService = "Noober Purple"
+ }
+
+ else if (ride[1].purpleRequested == true) {
+  levelOfService = "Noober Purple"
+}
+
+else if (ride[2].purpleRequested == true) {
+  levelOfService = "Noober Purple"
+}
+
+else if (ride[0].numberOfPassengers > 3) {
+  levelOfService = "Noober XL"
+}
+
+else {
+  levelOfService = "Noober X"
+}
 
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
